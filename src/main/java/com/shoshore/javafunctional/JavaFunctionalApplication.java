c9f1e8b8828f02e7324e9cb01c492ab026e7e6b5
Tapiwanashe Shoshore <tapiwanashe.shoshore@sovtech.com>
2023-02-16 12:34:55 +0200

    initial commit

---
+++ src/main/java/com/shoshore/javafunctional/JavaFunctionalApplication.java
@@ -0,0 +1,13 @@
+package com.shoshore.javafunctional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFunctionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoshore/javafunctional/JavaFunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoshore.javafunctional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFunctionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
